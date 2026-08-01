23f806e5f3b5f6f4bd9f632812cb15c0c3698d69
caioaex <caioexteckoetter0@gmail.com>
2026-08-01 19:50:38 -0300

    feat(web): setup inicial angular com tela de health check e design system

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HealthCheck } from './features/health-check/health-check';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'check',
+    component: HealthCheck
+  }
+];
